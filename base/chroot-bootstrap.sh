35d921454f3d39d1b70537379ff2aa60dad7cb38
Felipe Marinho <felipevm97@gmail.com>
2022-07-16 00:33:20 +0000

    chg: fix: add cloud-config and cloud-final systemd units

--- base/chroot-bootstrap.sh
+++ base/chroot-bootstrap.sh
@@ -132,6 +132,8 @@ chroot "$ROOTFS" systemctl enable sshd.service
 chroot "$ROOTFS" systemctl enable systemd-networkd.service
 chroot "$ROOTFS" systemctl enable systemd-resolved.service
 chroot "$ROOTFS" systemctl enable cloud-init.service
+chroot "$ROOTFS" systemctl enable cloud-config.service
+chroot "$ROOTFS" systemctl enable cloud-final.service
 chroot "$ROOTFS" systemctl enable chronyd.service
 chroot "$ROOTFS" systemctl mask tmp.mount
 chroot "$ROOTFS" systemctl set-default multi-user.target
